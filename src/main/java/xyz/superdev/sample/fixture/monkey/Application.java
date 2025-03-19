e76064e375e8f5b37b5bb11f910b24fe5e2d8119
mira.kim <mira@coyobi.com>
2025-03-19 22:39:50 +0900

    feat: init

---
+++ src/main/java/xyz/superdev/sample/fixture/monkey/Application.java
@@ -0,0 +1,13 @@
+package xyz.superdev.sample.fixture.monkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
